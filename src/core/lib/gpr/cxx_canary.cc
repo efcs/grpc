245b45aa0f18510b0ce02c36f75c6dc6bddd410e
Eric Fiselier <eric@efcs.ca>
2018-10-30 18:53:41 -0400

    fixes

---
+++ src/core/lib/gpr/cxx_canary.cc
@@ -0,0 +1,25 @@
+#include <cxxabi.h>
+
+
+struct Foo {
+  virtual void foo() = delete;
+  virtual void bar() = 0;
+  virtual ~Foo() {}
+};
+
+
+struct Bar : Foo {
+  virtual void foo() = delete;
+  virtual void bar() {}
+};
+
+volatile Foo *sink;
+
+using FnT = void(*)();
+volatile FnT fn_sink;
+
+bool my_test(Foo *f) {
+  sink = f;
+  fn_sink = &__cxxabiv1::__cxa_rethrow;
+  return fn_sink != nullptr;
+}
